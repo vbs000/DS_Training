4598214ed4c0437adbc45b142f6263809873fd73
vbs000 <vbs_000@126.com>
2021-03-09 22:32:51 +0800

    rev

--- src/main/java/org/leetcode/binary_search/Solution_69.java
+++ src/main/java/org/leetcode/binary_search/Solution_69.java
@@ -1,9 +1,21 @@
 package org.leetcode.binary_search;
 
+/**
+ * 69. x 的平方根
+ * 难度：简单
+ * 方法：二分查找
+ */
 public class Solution_69 {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了46.99%的用户
+     * 内存消耗：35.6 MB, 在所有 Java 提交中击败了47.88%的用户
+     * @param x
+     * @return
+     */
     public static int mySqrt(int x) {
         if(x == 0) return x;
         int l = 1, r = x, mid, sqrt;
