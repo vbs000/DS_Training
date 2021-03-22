6349584ae1fd449b90b62fae49b04e0f58921a65
vbs000 <vbs_000@126.com>
2021-03-22 22:48:40 +0800

    rev

--- src/main/java/org/test/practice/p0/DP_knapsack.java
+++ src/main/java/org/test/practice/p0/DP_knapsack.java
@@ -7,7 +7,7 @@ public class DP_knapsack {
         int[] s = new int[]{1,3,4};//空间
         int dp[] = new int[100001];
         int kind = s.length;//种类
-        int target = 6;//目标
+        int target = 6;
         for(int i=1; i<=target; i++) {//要求背包必须装满时，只是在预定义时，处理方式不同
             dp[i] = Integer.MAX_VALUE;
         }
